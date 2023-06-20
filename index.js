5385a1e4069b24aadf948a77adfb58c7ccb94449
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2023-06-20 16:57:44 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(names, checkName) {
+    return names.filter(function (name) {
+        return name.toLowerCase() == checkName.toLowerCase();
+    });
+}
+
+function fuzzyMatch(names, checkName) {
+    return names.filter(function (name) {
+        return name.slice(0, checkName.length) == checkName;
+    });
+}
+
+function matchName(names, checkName) {
+    return names.filter(function (driver) {
+        return driver.name == checkName;
+    });
+}
